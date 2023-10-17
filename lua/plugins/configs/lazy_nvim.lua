265bc3e33e291a39cb045ce0768464c69b71ff9f
zjtomoon <zjtomoon@outlook.com>
2023-10-17 19:47:03 +0800

    ssh 加速下载插件

--- lua/plugins/configs/lazy_nvim.lua
+++ lua/plugins/configs/lazy_nvim.lua
@@ -11,6 +11,19 @@ return {
     },
   },
 
+  git = {
+    -- defaults for the `Lazy log` command
+    -- log = { "-10" }, -- show the last 10 commits
+    log = { "-8" }, -- show commits from the last 3 days
+    timeout = 120, -- kill processes that take more than 2 minutes
+    -- url_format = "https://github.com/%s.git",
+		url_format = "git@github.com:%s.git", -- 重点
+    -- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
+    -- then set the below to false. This should work, but is NOT supported and will
+    -- increase downloads a lot.
+    filter = true,
+  },
+
   performance = {
     rtp = {
       disabled_plugins = {
